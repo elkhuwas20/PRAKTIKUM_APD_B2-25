39464d5e0e791a00a1a25c160de6dd6324002749
Nuril Akmal <akmalnuril65@gmail.com>
2025-10-07 22:02:18 +0800

    up

--- POSSTEST/post-test-apd-4/2509106074-NurilAkmal-PT-4.py
+++ POSSTEST/post-test-apd-4/2509106074-NurilAkmal-PT-4.py
@@ -1,54 +1,59 @@
 Nama_benar = "Nuril Akmal"
 NIM_benar = 2509106074
-pencobaan = 0
-max_pencobaan = 3
-while pencobaan < max_pencobaan:
+for percobaan in range(3):
     Nama = input("Masukkan Nama anda: ")
-    NIM  = int(input("Masukkan NIM anda : "))
-    if Nama == "Nuril Akmal" and NIM == 2509106074:
+    NIM = int(input("Masukkan NIM anda: "))
+    if Nama == Nama_benar and NIM == NIM_benar:
         print("Login berhasil. Selamat datang,", Nama)
         break
-    pencobaan += 1
-    if Nama == Nama_benar and NIM == NIM_benar:
-        print("Akses diterima")
-        break
     else:
-        print("Nama atau NIM salah silahkan coba lagi sebanyak", max_pencobaan - pencobaan, "kali.")
+        print(f"Nama atau NIM salah. Sisa percobaan: {2 - percobaan}")
 else:
     print("Akses ditolak. Terlalu banyak percobaan.")
     exit()
-print('===========Menu Pembelian Tiket============')
-print('1. Tiket Regular (Harga: Rp 50.000)')
-print('2. Tiket VIP     (Harga: Rp 100.000)')
-print('3. Tiket VVIP   (Harga: Rp 150.000)')
-pilihan = input('Pilih jenis tiket (1/2/3): ')
-jumlah = int(input('Masukkan jumlah tiket yang dibeli: '))
-if pilihan == '1':
-        harga = 50000
-        total_bayar = harga * jumlah
-        print('Total pembayaran Tiket Regular: Rp', total_bayar)
-elif pilihan == '2':
-        harga = 100000
-        total_bayar = harga * jumlah
-        print('Total pembayaran Tiket VIP: Rp', total_bayar)
-elif pilihan == '3':
-        harga = 150000
-        total_bayar = harga * jumlah
-        print('Total pembayaran Tiket VVIP: Rp', total_bayar)
-else:
-     print('Pilihan tidak valid.')
+total_bayar = 0
+jenis_tiket = ""
+for _ in range(99999999999):
+    print('\n===========Menu Pembelian Tiket============')
+    print('1. Tiket Regular (Rp 50.000)')
+    print('2. Tiket VIP     (Rp 100.000)')
+    print('3. Tiket VVIP    (Rp 150.000)')
+    print('4. Keluar')
+    pilihan = input('Pilih jenis tiket (1/2/3/4): ')
+    if pilihan == '4':
+        break
+    elif pilihan in ['1', '2', '3']:
+        jumlah = int(input('Masukkan jumlah tiket: '))     
+        if pilihan == '1':
+            harga = 50000
+            jenis_tiket = "Regular"
+        elif pilihan == '2':
+            harga = 100000
+            jenis_tiket = "VIP"
+        elif pilihan == '3':
+            harga = 150000
+            jenis_tiket = "VVIP" 
+        subtotal = harga * jumlah
+        total_bayar += subtotal
+        print(f'\n{jumlah} tiket {jenis_tiket} = Rp {subtotal:,}')
+        print(f'Total sementara: Rp {total_bayar:,}')
+    else:
+        print('Pilihan tidak valid!')
+print('\n=========== TOTAL PEMBAYARAN ===========')
+print(f'Total: Rp {total_bayar:,}')
 if total_bayar >= 300000:
-     diskon = total_bayar * 0.12
-total_bayar = total_bayar - diskon
-print('Anda mendapatkan diskon 12% sebesar: Rp', diskon)
-print('Total pembayaran setelah diskon: Rp', total_bayar)
-if total_bayar > 200000 and total_bayar <= 300000:
-        cashback = total_bayar * 0.08
-        print('Anda mendapatkan cashback 5% sebesar: Rp', cashback)
-if total_bayar > 150000 and total_bayar <= 200000:
-            print("Selamat Anda mendapatkan Poster Film Ekslusif")
+    diskon = total_bayar * 0.12
+    total_akhir = total_bayar - diskon
+    print(f'Diskon 12%: Rp {diskon:,}')
+    print(f'Total bayar: Rp {total_akhir:,}')
+elif total_bayar > 200000:
+    cashback = total_bayar * 0.08
+    print(f'Cashback 8%: Rp {cashback:,}')
+    print(f'Total bayar: Rp {total_bayar:,}')
+elif total_bayar > 150000:
+    print(f'Total bayar: Rp {total_bayar:,}')
+    print('Bonus: Poster Film Ekslusif')
 else:
-     print("Terima kasih telah memesan tiket di bioskop kami!")
-print("========================================")
-
-            
+    print(f'Total bayar: Rp {total_bayar:,}')
+print('Terima kasih telah memesan!')
+print('=========================================')
